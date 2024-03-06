5d0e635b287f8f83a21377818c105b012331aa76
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-06 08:43:15 -0300

    up

--- introducao/src/components/atividade00/MeusDados02.jsx
+++ introducao/src/components/atividade00/MeusDados02.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const MeusDados02 = ({ nome, curso, universidade }) => {
   return (
     <div>
+      <h1 className="titulo">Props</h1>
       <h1>Nome: {nome}</h1>
       <h1>Curso: {curso}</h1>
       <h1>Universidade: {universidade}</h1>
